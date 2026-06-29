421031a9bbf1c6c8246da75e66b01922d92f840b
Arno Feuz <a.feuz.inf24@stud.bbbaden.ch>
2026-06-29 18:38:08 +0200

    feat: add song model and data repository

---
+++ SongManager/SongManager/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongManager
+{
+    public class DataRepository
+    {
+        public List<Song> Songs { get; } = new List<Song>();
+        public List<Artist> Artists { get; } = new List<Artist>();
+        public List<Genre> Genres { get; } = new List<Genre>();
+        public List<difficulty> Difficulties { get; } = new List<difficulty>();
+    }
+}
---
+++ SongManager/SongManager/Models.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongManager
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Instrument { get; set; }
+        public string Notes { get; set; }
+        public string Sheet { get; set; }
+}
+
+    public class Artist
+    {
+     public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public enum difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+}
